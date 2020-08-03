71a8efe2db0f3f35c193ecb9825e98b9c7024554
endoakak <akendo2020@gmail.com>
2020-08-03 15:33:33 +0900

    バリデーションの修正　正規表現を変数にいれた　nick_nameとbirthdayをひとまとめにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nick_name, presence: true
-  validates :birthday, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/, message: "パスワードは英数字混合で入力してください" }
+  with_options presence: true do
+    validates :nick_name
+    validates :birthday
+  end
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: "パスワードは英数字混合で入力してください" }
+
+  ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  with_options presence: true, format: { with: ZENKAKU_REGEX, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'カタカナを使用してください' } do
+  KATAKANA_REGEX = /\A[ァ-ン]+\z/.freeze
+  with_options presence: true, format: { with: KATAKANA_REGEX, message: 'カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
