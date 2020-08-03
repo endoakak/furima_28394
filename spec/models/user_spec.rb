e15745347cc2a4b01e0ffafdcd5a896e7799addb
endoakak <akendo2020@gmail.com>
2020-08-03 11:25:18 +0900

    ユーザーモデルのテストコードの雛形作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#create" do
+    before do 
+      @user = FactoryBot.build(:user)
+    end
+      
+      it "正しく揃っていたら保存できる" do
+        
+      end
+      it "nick_nameが空だと保存できない" do
+        
+      end
+      it "emailが空だと保存できない" do
+        
+      end
+      it "emailが被っていると保存できない" do
+        
+      end
+      it "emailに@がないと保存できない" do
+        
+      end
+      it "passwordが空だと保存できない" do
+        
+      end
+      it "passwordが6文字以上でないと保存できない" do
+        
+      end
+      it "passwordが英数字混合出ないと保存できない" do
+        
+      end
+      it "passwordの確認が正しくないと保存できない" do
+        
+      end
+      it "last_nameが空だと保存できない" do
+        
+      end
+      it "first_nameが空だと保存できない" do
+        
+      end
+      it "last_nameが全角でないと保存できない" do
+        
+      end
+      it "first_nameが全角でないと保存できない" do
+        
+      end
+      it "last_name_kanaが空だと保存できない" do
+        
+      end
+      it "first_name_kanaが空だと保存できない" do
+        
+      end
+      it "last_name_kanaがカタカナでないと保存できない" do
+        
+      end
+      it "first_name_kanaがカタカナでないと保存できない" do
+        
+      end
+      it "birthdayが空だと保存できない" do
+        
+      end
+    end
+  end
 end
