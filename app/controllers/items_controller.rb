1efb678101cf39516fcc37e1d802f277c901d02d
endoakak <akendo2020@gmail.com>
2020-08-04 13:04:48 +0900

    商品出品機能（ビュー、アクション、ルーティング）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,32 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :text,
+      :category_id,
+      :condition_id,
+      :delivery_fee_id,
+      :delivery_from_id,
+      :delivery_day_id,
+      :price
+    ).merge(user_id: current_user.id)
+  end
 end
