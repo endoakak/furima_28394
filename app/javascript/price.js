e4dcc394c0910cc3fe79986256bd7e2e557d44fe
endoakak <akendo2020@gmail.com>
2020-08-04 18:10:31 +0900

    価格入力時の手数料などの表示

---
+++ app/javascript/price.js
@@ -0,0 +1,17 @@
+function price() {
+  const priceInput = document.getElementById("price_input")
+  const feeOutput = document.getElementById("add-tax-price")
+  const profitOutput = document.getElementById("profit")
+
+  priceInput.addEventListener("input", function() {
+    let price = this.value
+    let fee = Math.floor(price * 0.1)
+    let profit = price - fee
+    feeOutput.innerHTML = fee
+    profitOutput.innerHTML = profit
+
+    e.preventDefault()
+  })
+}
+
+window.addEventListener("load", price);
