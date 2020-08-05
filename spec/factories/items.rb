aa4913f14207255f6a807797353dd1aba5cf7b17
endoakak <akendo2020@gmail.com>
2020-08-05 11:23:42 +0900

    itemのダミーデータ作成でFakerを使わないように変更

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Lorem.word }
-    text { Faker::Lorem.sentence }
-    price { Faker::Number.between(from: 300, to: 9_999_999) }
-    category_id { Faker::Number.between(from: 2, to: 11) }
-    condition_id { Faker::Number.between(from: 2, to: 7) }
-    delivery_fee_id { Faker::Number.between(from: 2, to: 3) }
-    delivery_from_id { Faker::Number.between(from: 2, to: 48) }
-    delivery_day_id { Faker::Number.between(from: 2, to: 4) }
+    name { "商品" }
+    text { "商品の説明" }
+    price { 10000 }
+    category_id { 2 }
+    condition_id { 2 }
+    delivery_fee_id { 2 }
+    delivery_from_id { 2 }
+    delivery_day_id { 2 }
     association :user
   end
 end
