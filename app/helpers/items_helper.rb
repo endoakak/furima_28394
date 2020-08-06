39700b07143a76f893155641918aba4ea29bf934
endoakak <akendo2020@gmail.com>
2020-08-06 12:01:16 +0900

    ヘルパーメソッドis_availableの作成

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,17 @@
 module ItemsHelper
+  def is_available(item)
+    if !user_signed_in?
+      return false
+    end
+
+    if item.user.id == current_user.id
+      return false
+    end
+
+    if item.deal
+      return false
+    end
+
+    return true
+  end
 end
