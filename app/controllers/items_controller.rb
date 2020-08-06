3bb7055221ec115ea637c9e228d27920a6005e38
endoakak <akendo2020@gmail.com>
2020-08-06 15:31:33 +0900

    出品者以外の編集ページへのアクセスを阻害

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user.id == current_user.id
+      redirect_to item_path(@item.id)
+    end
   end
 
   def update
