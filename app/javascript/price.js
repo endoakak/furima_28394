bd6377955eb38e3263da3676107fa9f7be6fed0b
endoakak <akendo2020@gmail.com>
2020-08-06 14:48:24 +0900

    編集ページの表示機能の実装

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,9 +1,16 @@
 window.addEventListener("turbolinks:load", function() {
-  console.log("hello")
-  if (document.URL.match("/items/new")) {
+  if (document.getElementById("price_input")) {
     const priceInput = document.getElementById("price_input")
     const feeOutput = document.getElementById("add-tax-price")
     const profitOutput = document.getElementById("profit")
+
+    if (priceInput.value) {
+      let price = priceInput.value
+      let fee = Math.floor(price * 0.1)
+      let profit = price - fee
+      feeOutput.innerHTML = fee
+      profitOutput.innerHTML = profit
+    }
 
     priceInput.addEventListener("input", function() {
       let price = this.value
