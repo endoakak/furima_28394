23f0d8e4dc16d23243cfa4429cad975c7a5793de
endoakak <akendo2020@gmail.com>
2020-08-07 17:04:55 +0900

    DealShipmentのフォームオブジェクト

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,16 +1,17 @@
 class DealsController < ApplicationController
   def new
     @item = Item.find(params[:item_id])
+    @deal = DealShipment.new
   end
   
   def create
-    @deal = Deal.new(user_id: deal_params[:user_id], item_id: deal_params[:item_id])
+    @deal = DealShipment.new(deal_params)
+    @item = Item.find(deal_params[:item_id])
     if @deal.valid?
       pay_item
       @deal.save
       redirect_to root_path
     else
-      @item = Item.find(deal_params[:item_id])
       render :new
     end
 
@@ -19,13 +20,13 @@ class DealsController < ApplicationController
   private
 
   def deal_params
-    params.permit(:token, :item_id).merge(user_id: current_user.id)
+    params.permit(:token, :item_id, :post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id)
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: @deal.item.price,  # 商品の値段
+      amount: @item.price,  # 商品の値段
       card: deal_params[:token],    # カードトークン
       currency:'jpy'                 # 通貨の種類
     )
