8a97df28e9fbee35c936738d76c519561ac108cd
endoakak <akendo2020@gmail.com>
2020-08-07 14:45:36 +0900

    商品購入機能の実装

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,8 +1,33 @@
 class DealsController < ApplicationController
   def new
+    @item = Item.find(params[:item_id])
   end
   
   def create
+    @deal = Deal.new(user_id: deal_params[:user_id], item_id: deal_params[:item_id])
+    if @deal.valid?
+      pay_item
+      @deal.save
+      redirect_to root_path
+    else
+      @item = Item.find(deal_params[:item_id])
+      render :new
+    end
 
+  end
+
+  private
+
+  def deal_params
+    params.permit(:token, :item_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @deal.item.price,  # 商品の値段
+      card: deal_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類
+    )
   end
 end
--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,23 +1,21 @@
-window.addEventListener("turbolinks:load", function() {
-  if (document.getElementById("price_input")) {
-    const priceInput = document.getElementById("price_input")
-    const feeOutput = document.getElementById("add-tax-price")
-    const profitOutput = document.getElementById("profit")
+window.addEventListener("load", function() {
+  const priceInput = document.getElementById("price_input")
+  const feeOutput = document.getElementById("add-tax-price")
+  const profitOutput = document.getElementById("profit")
 
-    if (priceInput.value) {
-      let price = priceInput.value
-      let fee = Math.floor(price * 0.1)
-      let profit = price - fee
-      feeOutput.innerHTML = fee
-      profitOutput.innerHTML = profit
-    }
-
-    priceInput.addEventListener("input", function() {
-      let price = this.value
-      let fee = Math.floor(price * 0.1)
-      let profit = price - fee
-      feeOutput.innerHTML = fee
-      profitOutput.innerHTML = profit
-    })
+  if (priceInput.value) {
+    let price = priceInput.value
+    let fee = Math.floor(price * 0.1)
+    let profit = price - fee
+    feeOutput.innerHTML = fee
+    profitOutput.innerHTML = profit
   }
+
+  priceInput.addEventListener("input", function() {
+    let price = this.value
+    let fee = Math.floor(price * 0.1)
+    let profit = price - fee
+    feeOutput.innerHTML = fee
+    profitOutput.innerHTML = profit
+  })
 })
