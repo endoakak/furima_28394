028af6fcec8160c849378106c78f80249df16f2a
endoakak <akendo2020@gmail.com>
2020-08-10 16:12:04 +0900

    トークン生成に失敗してもフォームの情報を送信するように変更

--- app/forms/deal_shipment.rb
+++ app/forms/deal_shipment.rb
@@ -3,12 +3,15 @@ class DealShipment
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :token, :post_code, :prefecture_id, :city, :address, :building, :phone_number
 
+  validates :token, presence: { message: "Card information is invalid" }
   with_options presence: true do
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
     validates :address
-    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Maximun 11 digit number"}
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: "is invalid. Maximun 11 digit number" }
   end
 
   def save
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -20,16 +20,15 @@ const pay = () => {
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-
-        document.getElementById("number").removeAttribute("name");
-        document.getElementById("cvc").removeAttribute("name");
-        document.getElementById("exp_month").removeAttribute("name");
-        document.getElementById("exp_year").removeAttribute("name");
-
-        document.getElementById("charge-form").submit();
-        document.getElementById("charge-form").reset();
       } else {
       }
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").reset();
     });
   });
 };
