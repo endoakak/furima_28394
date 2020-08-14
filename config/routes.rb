96bfd34877a2b772cf03f923acd8f2bb0c65ed2b
endoakak <akendo2020@gmail.com>
2020-08-14 18:23:22 +0900

    cardsコントローラーnew,createアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   resources :users, only: [:show, :update]
   root to: "items#index"
   resources :items do
     resources :deals, only: [:new, :create]
   end
+  resources :cards, only: [:new, :create]
 end
