0041283dde12e9cab6eb639539bd6bd4c9d68525
endoakak <akendo2020@gmail.com>
2020-08-17 12:39:33 +0900

    フォームとcreateアクションの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    binding.pry
-    Comment.create(comment_params)
+    comment = Comment.new(comment_params)
     @comments = Comment.find_by(item_id: params[:item_id])
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:item_id, :text).merge(user_id: current_user.id)
+    params.require(:comment).permit(:text).merge(user_id: current_user.id).merge(item_id: params[:item_id])
   end
 end
