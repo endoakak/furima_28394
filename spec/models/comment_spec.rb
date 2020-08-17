4998e2097b7b413b161743ea42d56b9a4333737f
endoakak <akendo2020@gmail.com>
2020-08-17 16:08:33 +0900

    commentモデルの単体テストコード

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#create" do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    it "情報が揃っていれば保存できる" do
+      expect(@comment).to be_valid
+    end
+
+    it "コメントが空だと保存できない" do
+      @comment.text = ""
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Textを入力してください")
+    end
+  end
 end
